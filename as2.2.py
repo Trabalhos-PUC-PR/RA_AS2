480e2ee7b377bb19d451d65329ee86ab9ccc50af
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2021-04-26 19:14:55 -0300

    coloquei comentario e algoritmo

--- as2.2.py
+++ as2.2.py
@@ -19,21 +19,53 @@ sobre ela e use na solução. Pode usar a mesma função ao algoritmo também
 
 ABS() serve para pegar o valor absoluto (módulo) de um número, é basicamente isso
 '''
-a = int(input("Digite o valor a: "))
-b = int(input("Digite o valor b: "))
-c = int(input("Digite o valor c: "))
+'''
+#VARIAVEIS
+valor_a = 0
+valor_b = 0
+valor_c = 0
+a_modulo = 0
+b_modulo = 0
+c_modulo = 0
 
-aABS = abs(b - c)
-bABS = abs(a - c)
-cABS = abs(a - b)
+# PROGRAMA
+valor_a <- leia("digite o valor A")
+valor_b <- leia("digite o valor B")
+valor_c <- leia("digite o valor C")
 
-def regra(abs, prim, seg, terc):
-    if(abs < prim and prim < (seg + terc)):
-        print(prim,"é um número válido! ")
-    else:
-        print(prim,"não é um número válido! ")
-        
+a_modulo = modulo(b - c)
+b_modulo = modulo(a - c)
+c_modulo = modulo(a - b)
+
+funcao regra(absoluto, primeiro, segundo, terceiro):
+    se absoluto < primeiro and primeiro < (segundo + terceiro):
+    
+        imprima(primeiro,"é um número válido! ")
+    senao:
+        imprima(primeiro,"não é um número válido! ")
+    fimse
+
 regra(aABS, a, b, c)
 regra(bABS, b, a, c)
 regra(cABS, c, a, b)
+
 exit()
+''' #entrada dos valores
+a = int(input("Digite o valor a: ")) 
+b = int(input("Digite o valor b: "))
+c = int(input("Digite o valor c: "))
+
+aABS = abs(b - c) # primeiro modulo
+bABS = abs(a - c) # segundo modulo
+cABS = abs(a - b) # terceiro modulo
+
+def regra(abs, prim, seg, terc): #cria uma função chamada regra pra verificar os angulos (basicamente se um ta errado, todos estão)
+    if(abs < prim and prim < (seg + terc)): #se ta conforme a regra
+        print(prim,"é um número válido! ") # é um valor valido
+    else:
+        print(prim,"não é um número válido! ") # se não, não era valido
+        
+regra(aABS, a, b, c) #chamando a função com esses valores
+regra(bABS, b, a, c) # denovo
+regra(cABS, c, a, b) # e denovo
+exit() # ai acaba o codigo
